1e673f614bb32b5fa7ab96a08c93c2ef9b4300c9
harshasupare <74301292+harshasupare@users.noreply.github.com>
2021-04-28 19:14:09 +0530

    Add files via upload

---
+++ hint.js
@@ -0,0 +1,8 @@
+let name1="John";//JO
+let name2="John Doe";//JD
+let name3="John Doe Smith";//JS
+
+//I need to get the first two characters of the string
+let First Two characters=name1.substring(0,2);//JO
+
+//I need to convert a string to all capsin javascript
